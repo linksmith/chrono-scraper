016e314dd07493f310463d8a80a275bb7cc5bb9f
Jeremy CS <info@linksmith.nl>
2024-01-08 17:10:24 +0100

    FastAPI search added

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -10,6 +10,25 @@ def read_root():
     return {"Hello": "World"}
 
 
+@app.get("/search")
+def search(query: str):
+    # Conduct search with filters
+    search_params = {
+        # 'filter': f'timestamp >= {min_date_timestamp} AND timestamp < {max_date_timestamp}',
+        'attributesToHighlight': ['text'],
+        'attributesToCrop': ['text'],
+        'cropLength': 36,
+        'highlightPreTag': "**",
+        'highlightPostTag': "**",
+        'limit': 10,
+        # 'offset': st.session_state.offset,
+        # 'showRankingScore': True
+    }
+    
+    results = search(query, search_params)
+    
+    return results
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
@@ -20,3 +39,4 @@ def read_item(item_id: int, q: Union[str, None] = None):
 #     arg1, arg2 = None
 #     some_task.delay(arg1, arg2)
 #     return {"status": "Task started"}
+
