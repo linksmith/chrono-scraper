62595eb04bd1673183515f7166ae3c0765f9af3f
Jeremy CS <info@linksmith.nl>
2024-01-09 20:34:09 +0100

    Removed old setup

--- strapi/src/api/domain/controllers/domain.ts
+++
@@ -1,7 +0,0 @@
-/**
- * domain controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::domain.domain');
--- strapi/src/api/domain/services/domain.ts
+++
@@ -1,7 +0,0 @@
-/**
- * domain service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::domain.domain');
